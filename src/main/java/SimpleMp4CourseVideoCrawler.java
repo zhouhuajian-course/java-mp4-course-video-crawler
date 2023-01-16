996e29b457a787cc61675c4b349be414c4cb6017
zhouhuajian <zhouhuajian@company.com>
2023-01-16 15:46:11 +0800

    增加项目依赖

---
+++ src/main/java/SimpleMp4CourseVideoCrawler.java
@@ -0,0 +1,9 @@
+/*********************************************************
+                简单的mp4课程视频网络爬虫
+                 author: zhouhuajian
+ *********************************************************/
+public class SimpleMp4CourseVideoCrawler {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
